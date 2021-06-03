ce3ddf99d67a95df3c6f33f1ff6a9736410061a2
Karl Keller <kgk86@hotmail.com>
2021-06-03 11:24:52 +0200

    rake db:migrate updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_112055) do
+ActiveRecord::Schema.define(version: 2021_06_03_112320) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating", default: 0, null: false
   end
 
 end
